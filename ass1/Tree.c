57998a38811060c9fe5e48da830fedd89f727057
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-10-22 12:07:29 +1100

    Fixed up style using clang formatter

--- ass1/Tree.c
+++ ass1/Tree.c
@@ -191,9 +191,8 @@ static int getHeight(Node n) {
  */
 static Node TreeRotateRight(Node n) {
 	// The following code was adapted from the lecture code on tree rotations
-	Node current = n->left;  // set up a new root and make it point to the left tree
-	if (n == NULL || current == NULL)  // if trees our empty return our original tree
-	{
+	Node current = n->left;
+	if (n == NULL || current == NULL) {
 		return n;
 	} else {
 		n->left = current->right;
@@ -210,20 +209,19 @@ static Node TreeRotateRight(Node n) {
  */
 static Node TreeRotateLeft(Node n) {
 	// The following code was adapted from the lecture code on tree rotations
-	Node current = n->right;  // setup a current node which will point to n->right
-	if (n == NULL || current == NULL)  // if the node, or current doesn't exist return n
-	{
+	Node current = n->right;
+	if (n == NULL || current == NULL) {
 		return n;
 	} else {
-		n->right = current->left;  // assigns n->right with the
+		n->right = current->left;
 		current->left = n;
 		return current;
 	}
 }
 
 /**
- * @brief - The following function is a helper function for TreeInsert and will basically
- * perform the insertion. Inserts a record into the tree. 
+ * @brief - The following function is a helper function for TreeInsert and will 
+ * basically perform the insertion. Inserts a record into the tree. 
  * 
  * @param t - Tree t
  * @param rec - Record rec 
@@ -232,8 +230,8 @@ static Node TreeRotateLeft(Node n) {
  * @return Node - node to insert 
  */
 static Node doTreeInsert(Tree t, Record rec, Node n, bool *result) {
-	if (n == NULL)  // if the tree doesn't exist, create it and return true
-	{
+	// if the tree doesn't exist, create it and return the new node
+	if (n == NULL) {
 		Node newNode = createNewNode(rec);
 		return newNode;
 	}
@@ -241,11 +239,11 @@ static Node doTreeInsert(Tree t, Record rec, Node n, bool *result) {
 	int compare = t->compare(rec, n->rec);
 
 	// the following code will insert into correct position
-	if (compare == 0)  // if the rec and our rec are the same then no need to insert
-	{
+	// if the rec and our rec are the same then no need to insert
+	if (compare == 0) {
 		*result = false;
 		return n;
-	} else if (compare < 0) {  // if the rec is is less than our rec then traverse the left section
+	} else if (compare < 0) {  // if the rec less than our rec go left
 		n->left = doTreeInsert(t, rec, n->left, result);
 	} else {  // else we must traverse the right section
 		n->right = doTreeInsert(t, rec, n->right, result);
@@ -254,19 +252,18 @@ static Node doTreeInsert(Tree t, Record rec, Node n, bool *result) {
 	int lheight = getHeight(n->left);
 	int rheight = getHeight(n->right);
 
-	// the following code will rebalance the inserted node and is an adapted version of the lecture code on avl trees
-	if ((lheight - rheight) > 1)  // if the balance factor is unbalanced then proceed below
-	{
+	// the following code will rebalance the inserted node and is an adapted
+	// version of the lecture code on avl trees
+	// if the balance factor is unbalanced then proceed below
+	if ((lheight - rheight) > 1) {
 		int compare_left = t->compare(rec, n->left->rec);
-		if (compare_left > 0)  // this means the item will be positioned to the right of the t->left section, to balance this we need to rotate it to the left
-		{
+		if (compare_left > 0) {
 			n->left = TreeRotateLeft(n->left);
 		}
-		n = TreeRotateRight(n);  // after rotating to the left we need to rotate it to the right to ensure balance
+		n = TreeRotateRight(n);
 	} else if ((rheight - lheight) > 1) {
 		int compare_right = t->compare(rec, n->right->rec);
-		if (compare_right < 0)  // this is a RL imbalance, therfore we need to first rotate to the right and then left
-		{
+		if (compare_right < 0) {
 			n->right = TreeRotateRight(n->right);
 		}
 		n = TreeRotateLeft(n);
@@ -288,20 +285,21 @@ static Record doTreeSearch(Tree t, Node n, Record rec) {
 		return NULL;
 	}
 
-	int compare = t->compare(rec, n->rec);  // make the comparison between the record of interest and our record
+	// make the comparison between the record of interest and our record
+	int compare = t->compare(rec, n->rec);
 
-	if (compare < 0)  // if record of interest is less than current record, traverse left section of tree
-	{
+	if (compare < 0) {
 		return doTreeSearch(t, n->left, rec);
-	} else if (compare > 0) {  // if record of interest is more than current record, traverse right section of tree
+	} else if (compare > 0) {
 		return doTreeSearch(t, n->right, rec);
-	} else {  // if we found the record return the current record
+	} else {
 		return n->rec;
 	}
 }
 
 /**
- * @brief - The following function is the helper function for TreeSearchBetween. 
+ * @brief - The following function is the helper function for 
+ * TreeSearchBetween.  
  * 
  * @param t - Tree t
  * @param n - Node n 
@@ -321,18 +319,18 @@ static void doTreeSearchBetween(Tree t, Node n, Record lower, Record upper,
 
 	// to have an inorder traversal, left -> root -> right
 
-	if (compare_lower < 0)  // if the record is higher than the lower bound, traverse left section
-	{
+	// if the record is higher than the lower bound, traverse left section
+	if (compare_lower < 0) {
 		doTreeSearchBetween(t, n->left, lower, upper, l);
 	}
 
-	if (compare_higher >= 0 && compare_lower <= 0)  // else go ahead and add to the list!
-	{
+	// go ahead and add to the list!
+	if (compare_higher >= 0 && compare_lower <= 0) {
 		ListAppend(l, n->rec);
 	}
 
-	if (compare_higher > 0)  // if the record is higher than the higher bound, traverse right section
-	{
+	// if the record is higher than the higher bound, traverse right section
+	if (compare_higher > 0) {
 		doTreeSearchBetween(t, n->right, lower, upper, l);
 	}
 }
@@ -346,15 +344,15 @@ static void doTreeSearchBetween(Tree t, Node n, Record lower, Record upper,
  * @param next - Record *next 
  */
 static void doTreeNext(Tree t, Node n, Record rec, Record *next) {
-	if (n == NULL) return;
+	if (n == NULL)
+		return;
 
 	int compare = t->compare(rec, n->rec);
 
-	if (compare > 0)  // if the record of interest is higher than our current record then traverse right subtree
-	{
+	if (compare > 0) {
 		return doTreeNext(t, n->right, rec, next);
-	} else {  // if the record of interest is lower than our current record then traverse left subtree
-		*next = n->rec;  // update the next address to hold the current record
+	} else {
+		*next = n->rec;
 		return doTreeNext(t, n->left, rec, next);
 	}
 }
