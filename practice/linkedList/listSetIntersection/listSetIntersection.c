e698c0fe3de494cc15b497f94bb2b8ade483403b
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-11-22 16:10:49 +1100

    Finished exercise 7 for LL

--- practice/linkedList/listSetIntersection/listSetIntersection.c
+++ practice/linkedList/listSetIntersection/listSetIntersection.c
@@ -1,8 +1,39 @@
 
 #include "list.h"
 
+bool checkInList(List l, int value)
+{
+	for (Node current = l->head; current != NULL; current = current->next)
+	{
+		if (current->value == value)
+		{
+			return true; 
+		}
+	}
+	return false; 
+}
+
+void prepend(List l, int value)
+{
+	Node new = newNode(value); 
+
+	new->next = l->head; 
+	l->head = new; 
+}
+
 List listSetIntersection(List s1, List s2) {
 	// TODO
-	return newList();
+	List new = newList(); // create a new list 
+	
+	for (Node current = s1->head; current != NULL; current = current->next)
+	{
+		if (checkInList(s2, current->value))
+		{
+			prepend(new, current->value); 
+		}
+	}
+	// new = listSortedCopy(new); 
+
+	return new;
 }
 
