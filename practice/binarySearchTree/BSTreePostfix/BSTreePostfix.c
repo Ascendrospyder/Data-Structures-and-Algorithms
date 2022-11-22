4a5e8515d17a8cc5308e0f25d41096aeabd48a62
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-11-22 20:09:36 +1100

    Finished prac 2 for bst

--- practice/binarySearchTree/BSTreePostfix/BSTreePostfix.c
+++ practice/binarySearchTree/BSTreePostfix/BSTreePostfix.c
@@ -3,7 +3,15 @@
 
 #include "BSTree.h"
 
-void BSTreePostfix(BSTree t) {
+void BSTreePostfix(BSTree t) 
+{
 	// TODO
+	if (t == NULL)
+	{
+		return; 
+	}
+	BSTreePostfix(t->left); 
+	BSTreePostfix(t->right); 
+	printf("%d ", t->value); 
 }
 
