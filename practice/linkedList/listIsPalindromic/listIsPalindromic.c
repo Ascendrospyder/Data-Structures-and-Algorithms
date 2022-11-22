833f7157f56468baf6c1cebde29d6773d7846bbd
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-11-22 16:53:26 +1100

    completed practice 8

--- practice/linkedList/listIsPalindromic/listIsPalindromic.c
+++ practice/linkedList/listIsPalindromic/listIsPalindromic.c
@@ -1,8 +1,42 @@
 
 #include "list.h"
 
+int countNodes(List l)
+{
+	int counter = 0; 
+	for (Node current = l->first; current != NULL; current = current->next)
+	{
+		counter++; 
+	}
+	return counter; 
+}
+/*
+	steps:
+	1. step through the start to the end of the list and compare the value with 
+	the last to first 
+*/
 bool listIsPalindromic(List l) {
 	// TODO
-	return false;
+	int counter = 0; 
+	Node start = l->first; 
+	Node end = l->last; 
+	while (start != NULL && end != NULL)
+	{
+		if (start->value == end->value)
+		{
+			counter++; 
+		}	
+		start = start->next; 
+		end = end->prev; 
+	}
+
+
+	if (counter == countNodes(l))
+	{
+		return true; 
+	} else {
+		return false;
+	}
+
 }
 
