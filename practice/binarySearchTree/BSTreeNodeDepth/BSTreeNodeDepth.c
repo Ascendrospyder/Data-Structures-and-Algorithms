abb9f5067c7ed368dd39319ab48863d693df15bc
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-11-23 12:48:10 +1100

    Finished exercise on bst

--- practice/binarySearchTree/BSTreeNodeDepth/BSTreeNodeDepth.c
+++ practice/binarySearchTree/BSTreeNodeDepth/BSTreeNodeDepth.c
@@ -3,7 +3,29 @@
 
 #include "BSTree.h"
 
-int BSTreeNodeDepth(BSTree t, int key) {
-	return 0;
+int BSTreeNodeDepth(BSTree t, int key) 
+{
+	if (t == NULL)
+	{
+		return -1; 
+	} else if (key == t->value) {
+		return 0; 
+	} else if (key < t->value) {
+		int depth = BSTreeNodeDepth(t->left, key); 
+		if (depth == -1)
+		{
+			return -1; 
+		} else {
+			return depth + 1;
+		}
+	} else {
+		int depth = BSTreeNodeDepth(t->right, key); 
+		if (depth == -1)
+		{
+			return -1; 
+		} else {
+			return depth + 1;
+		}
+	} 
 }
 
