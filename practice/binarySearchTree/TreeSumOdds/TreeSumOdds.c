8fc85d498a463f95cf8534df128d84a09b002676
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-11-23 18:03:08 +1100

    Finished prac 6

--- practice/binarySearchTree/TreeSumOdds/TreeSumOdds.c
+++ practice/binarySearchTree/TreeSumOdds/TreeSumOdds.c
@@ -1,9 +1,26 @@
 
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "tree.h"
 
-int TreeSumOdds(Tree t) {
-	return 0;
+/*
+	steps : 
+	1. for the root verify if the value is odd, if it is, then go ahead and 
+	add value to our sum 
+	2. then go to the left of the tree and verify that
+	3. then the right 
+
+*/
+int TreeSumOdds(Tree t)
+{
+	if (t == NULL)
+	{
+		return 0; 
+	} else if ((t->value % 2) != 0) {
+		return t->value + TreeSumOdds(t->left) + TreeSumOdds(t->right); 
+	} else {
+		return TreeSumOdds(t->left) + TreeSumOdds(t->right); 
+	}
 }
 
