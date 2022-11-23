d90b0a1930340f22ce9c029385c4b727e42b560e
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-11-23 18:07:17 +1100

    Tested another method on how to do it

--- practice/binarySearchTree/TreeSumOdds/TreeSumOdds.c
+++ practice/binarySearchTree/TreeSumOdds/TreeSumOdds.c
@@ -17,7 +17,7 @@ int TreeSumOdds(Tree t)
 	if (t == NULL)
 	{
 		return 0; 
-	} else if ((t->value % 2) != 0) {
+	} else if ((t->value % 2) == 1) {
 		return t->value + TreeSumOdds(t->left) + TreeSumOdds(t->right); 
 	} else {
 		return TreeSumOdds(t->left) + TreeSumOdds(t->right); 
